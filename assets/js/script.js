96413485c780ab0933f9e3cd0b61d15ab7e2bb10
Victor hugo <victr.hf@gmail.com>
2025-09-28 15:16:18 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+export default class Game {
+    constructor(){
+
+    }
+
+    startGame(){
+
+    }
+
+    moveTarget(){
+        
+    }
+
+    addHTML(){
+
+    }
+
+    bossGenerator(){
+
+    }
+
+    ballonGenerator(){
+
+    }
+
+    shoot(){
+        
+    }
+}
